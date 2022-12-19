215d64840d2f6329067e84e9a09c6f133bf14a6a
mgonzalz <mgonzgarc@myuax.com>
2022-12-19 22:45:59 +0100

    cambios

--- estrellas_regulares.py
+++ estrellas_regulares.py
@@ -4,12 +4,13 @@ def estrella_regular(lados):
     begin_fill() # comienza a rellenar
     for i in range(lados):
         forward(200) # avanza 200 pasos
-        left(180-180/lados)
+        left(180-180/lados) # gira 180 - 180 / lados grados
+
     end_fill()
     done()
 def inicio():
     print("Esto es una estrella regular")
-    try:
+    try: # comprobar si es un numero
         lados = int(input("¿Cuántos lados desea que tenga la estrella?: "))
         estrella_regular(lados)
     except ValueError:
